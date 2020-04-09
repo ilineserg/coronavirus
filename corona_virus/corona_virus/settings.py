889521f5e2e907e17c14af953716e1889aa01abf
Sergey ILIN <ilineserg@gmail.com>
2020-04-09 15:42:26 +0300

    add db settings

--- corona_virus/corona_virus/settings.py
+++ corona_virus/corona_virus/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'corona_virus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        'HOST': "127.0.0.1",
+        'NAME': "corona",
+        'USER': "corona",
+        'PASSWORD': "corona",
+        'PORT': 5432,
     }
 }
 
